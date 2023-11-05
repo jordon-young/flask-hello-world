89c1e6fbbcf90b04d43fdf203939987456192569
Jordon Young <64931019+jordon-young@users.noreply.github.com>
2023-11-05 23:27:47 +0000

    Add db_create

--- app.py
+++ app.py
@@ -3,12 +3,35 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+DATABASE_URL = "postgres://flask_hello_world_fnxg_user:SqJ6U11VXSvKXrrYBAwNy1CZDYuwEo3x@dpg-cl42172uuipc738p5qk0-a/flask_hello_world_fnxg"
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.connect("postgres://flask_hello_world_fnxg_user:SqJ6U11VXSvKXrrYBAwNy1CZDYuwEo3x@dpg-cl42172uuipc738p5qk0-a/flask_hello_world_fnxg")
+    conn = psycopg2.connect(DATABASE_URL)
     conn.close()
     return "Database Connection Successful"
+
+
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    cur.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+
+
