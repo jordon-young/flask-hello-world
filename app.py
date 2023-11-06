3ab997d1e7bcbaae168055c60e2906885eb6ca0d
Jordon Young <64931019+jordon-young@users.noreply.github.com>
2023-11-06 01:34:39 +0000

    Add db_insert

--- app.py
+++ app.py
@@ -32,4 +32,20 @@ def db_create():
     conn.close()
     return "Basketball Table Successfully Created"
 
+@app.route('/db_insert')
+def db_insert():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Populated"
+
 
